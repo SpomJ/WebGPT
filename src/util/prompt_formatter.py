06f1d147453a65d13ed38d54326839d03dd8acbf
SpomJ <mihail_a_m@mail.ru>
2024-03-10 16:30:33 +0300

    Distinguish between narrators when parsing prompts

---
+++ src/util/prompt_formatter.py
@@ -0,0 +1,15 @@
+ROLE_TOKEN = {
+    'system': '@[SYS]',
+    'user': '@[USR]',
+    'assistant': '@[WEB]',
+    'bot': '@[BOT]'
+}
+S_END = '@[END]'
+
+def fmt(msgs: list):
+    O = ''
+    for msg in msgs:
+      O += ROLE_TOKEN[msg['role']]
+      O += msg['content']
+      O += S_END
+    return O
